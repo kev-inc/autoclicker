9e413354d38104c7f871344e061da7c4c522e2ea
kev-inc <kevinchanxy@gmail.com>
2021-12-30 14:10:20 +0800

    windows fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ target_png = 'images/backBtn.png'
 
 def nav_to_image():
     position = pt.locateCenterOnScreen(target_png, grayscale=True, confidence=0.9)
-    pt.doubleClick(position[0]/2 + random.uniform(-5, 5) , position[1]/2 + random.uniform(-5, 5))
+    pt.doubleClick(position[0] + random.uniform(-5, 5) , position[1] + random.uniform(-5, 5))
 
 print("Starting in 3 seconds... Please ensure that the whole game is visible on screen.")
 time.sleep(3)
